b6d9b7fd156e515090e751f07e0b4df49095d5fc
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2019-08-28 20:43:24 -0700

    dev stuff

--- js/app.js
+++ js/app.js
@@ -1,2 +1,56 @@
-const storeName = '';
+const storeName = 'Filly H';
 document.title = `${storeName} Grocery Store`;
+
+// console.log('what is in document:', document.all[23]);
+
+// console.log('what is in document:', document.all);
+
+// console.log(document.getElementsByTagName('p'));
+
+let h1Header = document.getElementById('mainIntro');
+console.log(h1Header);
+
+h1Header.innerHTML='Welcome';
+
+const getAllNavListClasses = document.querySelectorAll('.nav_list_item');
+
+console.log(getAllNavListClasses);
+
+const getIconCredit = document.querySelector('#iconsCredit');
+console.log(getIconCredit);
+
+// const example = document.querySelector('.nav_list_item');
+// console.log(example);
+
+// const example = document.getElementsByClassName('nav_list_item');
+// console.log(example);
+
+const imageExample = document.querySelector('img[alt="cherry icon"]');
+
+console.log(imageExample);
+
+const magicButton = document.getElementById('magic');
+// console.log(magicButton);
+
+// magicButton.addEventListener('click', () => {
+//     magicButton.className = 'myPoint'
+//     console.log(magicButton);
+// })
+
+magicButton.addEventListener('mouseover', () => {
+    magicButton.className = 'myPoint'
+    console.log(magicButton);
+})
+
+
+// magicButton.addEventListener('mouseleave', () =>{
+//     magicButton.className = '';
+// })
+
+magicButton.addEventListener('mouseleave', () =>{
+    magicButton.classList.remove("myPoint");
+})
+
+document.addEventListener('keydown', () =>{
+    console.log(event.keyCode);
+})
